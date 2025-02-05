cb4e7413bf1b1d577be8c96f4b9402853e88c9e5
AlfredoRiveraaa <alfrexri550@gmail.com>
2025-02-04 20:26:39 -0600

    Ejercicio 2 realizado en index.php

--- practicas/p04/index.php
+++ practicas/p04/index.php
@@ -29,6 +29,52 @@
         echo '<li>$_element1 es válida porque inicia con guión bajo.</li>';
         echo '<li>$house*5 es inválida porque el símbolo * no está permitido.</li>';
         echo '</ul>';
+
+        // Liberar las variables
+        unset($_myvar, $_7var, $myvar, $var7, $_element1);
+    ?>
+
+    <!-- Ejercicio 2 -->
+    <h2>Ejercicio 2</h2>
+    <p>Proporcionar los valores de $a, $b, $c como sigue:</p>
+    <p>$a = “ManejadorSQL”;</p>
+    <p>$b = 'MySQL’;</p>
+    <p>$c = &$a;</p>
+    <?php
+        // Primera asignación de valores
+        $a = "ManejadorSQL";
+        $b = 'MySQL';
+        $c = &$a; // $c es una referencia a $a
+
+        echo '<h4>Valores iniciales:</h4>';
+        echo '<ul>';
+        echo '<li>$a: '; var_dump($a); echo '</li>';
+        echo '<li>$b: '; var_dump($b); echo '</li>';
+        echo '<li>$c: '; var_dump($c); echo '</li>';
+        echo '</ul>';
+
+        echo '<h4>Agrega al código las siguientes asignaciones:</h4>';
+        echo '<p>$a = “PHP server”;</p>';
+        echo '<p>$b = &$a;</p>';
+        // Segunda asignación de valores
+        $a = "PHP server";
+        $b = &$a; // Ahora $b también referencia a $a
+
+        echo '<h4>Valores después de la reasignación:</h4>';
+        echo '<ul>';
+        echo '<li>$a: '; var_dump($a); echo '</li>';
+        echo '<li>$b: '; var_dump($b); echo '</li>';
+        echo '<li>$c: '; var_dump($c); echo '</li>';
+        echo '</ul>';
+
+        // Explicación del comportamiento
+        echo '<h4>Explicación:</h4>';
+        echo '<p>Inicialmente, $c es una referencia a $a, lo que significa que cualquier cambio en $a también afectará a $c.</p>';
+        echo '<p>Cuando reasignamos $a a "PHP server", tanto $a como $c reflejan el nuevo valor, ya que $c sigue siendo una referencia a $a.</p>';
+        echo '<p>Luego, al hacer <code>$b = &$a;</code>, ahora $b también es una referencia a $a, por lo que cualquier cambio en $a afectará también a $b y $c.</p>';
+
+        // Liberar variables
+        unset($a, $b, $c);
     ?>
 </body>
 </html>
